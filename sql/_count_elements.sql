df28d868eee283866c4d11f8ad21f74581d2fc6e
Oscar Fonts <oscar.fonts@gmail.com>
2011-05-05 14:45:43 +0200

    Direcciones postales en construcciones sacadas de cat_11

---
+++ sql/_count_elements.sql
@@ -0,0 +1,17 @@
+﻿-- tipos de eje (3)
+--select distinct ttggss, descripcion, count(*) from ejes_nombre group by ttggss, descripcion order by count desc;
+
+-- tipos de constru (14) !! SIN alturas positivas !!
+--select distinct constru, tipo, count(*) from construcciones where not tiene_alturas_positivas group by constru, tipo order by count desc;
+
+-- tipos de elemlin (11)
+--select distinct dict_ttggss.ttggss, dict_ttggss.descripcion, count(*) from dict_ttggss, elemlin where dict_ttggss.ttggss = elemlin.ttggss group by dict_ttggss.ttggss, dict_ttggss.descripcion order by count desc;
+
+-- tipos de elempun (3)
+--select distinct dict_ttggss.ttggss, dict_ttggss.descripcion, count(*) from dict_ttggss, elempun where dict_ttggss.ttggss = elempun.ttggss group by dict_ttggss.ttggss, dict_ttggss.descripcion order by count desc;
+
+-- tipos de limite (1)
+--select distinct dict_ttggss.ttggss, dict_ttggss.descripcion, count(*) from dict_ttggss, limites where dict_ttggss.ttggss = limites.ttggss group by dict_ttggss.ttggss, dict_ttggss.descripcion order by count desc;
+
+-- tipos de elemtex (10)
+--select distinct dict_ttggss.ttggss, dict_ttggss.descripcion, count(*) from dict_ttggss, elemtex where dict_ttggss.ttggss = elemtex.ttggss group by dict_ttggss.ttggss, dict_ttggss.descripcion order by count desc;
--- sql/views.sql
+++ sql/views.sql
@@ -1,34 +1,59 @@
-CREATE OR REPLACE VIEW ejes_nombre AS
+--
+-- Ejes con sus nombres
+--
+﻿CREATE OR REPLACE VIEW ejes_nombre AS
  SELECT DISTINCT ejes.gid, ejes.the_geom, calles.nombre, dict_ttggss.ttggss, dict_ttggss.descripcion as tipo
    FROM ejes
    LEFT OUTER JOIN calles
      ON ejes.via = calles.id, dict_ttggss
   WHERE dict_ttggss.ttggss = ejes.ttggss;
-
   
 INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
   SELECT '', '', 'ejes_nombre', 'the_geom', ST_CoordDim(the_geom), ST_SRID(the_geom), GeometryType(the_geom)
   FROM ejes_nombre LIMIT 1;
 
-CREATE OR REPLACE VIEW parcela_direccion AS 
-   SELECT DISTINCT parcela.gid, parcela.the_geom, parcela.refcat,
-          CASE WHEN parcela.numero <> 0 THEN (calles.nombre || ', ' || parcela.numero) ELSE calles.nombre END AS direccion
-     FROM parcela
-     LEFT OUTER JOIN calles
-       ON parcela.via = calles.id;
+SELECT 
+, *
+FROM constru
 
-INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
-  SELECT '', '', 'parcela_direccion', 'the_geom', ST_CoordDim(the_geom), ST_SRID(the_geom), GeometryType(the_geom)
-  FROM parcela_direccion LIMIT 1;
-
--- TODO sacar direcciones mas chulas, con numeros de policia
+---
+--- Construcciones con sus direcciones y si tienen alturas positivas
+---
 CREATE OR REPLACE VIEW construcciones AS 
-   SELECT DISTINCT constru.gid, constru.the_geom, constru.constru, dict_constru.desc as tipo, parcela_direccion.direccion
-     FROM constru
-     LEFT OUTER JOIN dict_constru
-       ON constru.constru = dict_constru.id
-     LEFT OUTER JOIN parcela_direccion
-       ON constru.refcat = parcela_direccion.refcat;
+   SELECT DISTINCT constru.gid, constru.the_geom, constru.refcat, constru.constru, dict_constru.desc as tipo,
+     CASE
+       WHEN nombre_via = ''
+       THEN initcap(nombre_paraje)
+       ELSE dict_tipus_via.desc || ' ' || INITCAP(nombre_via) ||
+         CASE
+	   WHEN primer_numero_policia <> 0
+           THEN ', ' || primer_numero_policia ||
+	     CASE
+	       WHEN primera_letra <> ''
+	       THEN primera_letra
+	       ELSE ''
+	     END
+	   ELSE ''
+         END ||
+         CASE
+           WHEN segundo_numero_policia <> 0
+	   THEN '-' || segundo_numero_policia ||
+	     CASE
+	       WHEN segunda_letra <> ''
+	       THEN segunda_letra
+	       ELSE ''
+	     END
+	   ELSE ''
+         END
+     END AS direccion,
+        (constru.constru ~ E'(?:^|\\+)(L{1,3}|(XL|X{1,3})|IX|VI{1,3}|I?V|I{1,3})') as tiene_alturas_positivas
+   FROM constru
+   LEFT OUTER JOIN dict_constru
+     ON constru.constru = dict_constru.id
+   LEFT OUTER JOIN cat_11
+     ON constru.refcat=cat_11.parcela_catastral
+   LEFT OUTER JOIN dict_tipus_via
+    ON dict_tipus_via.id = cat_11.tipo_via;
 
 INSERT INTO geometry_columns(f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type")
   SELECT '', '', 'construcciones', 'the_geom', ST_CoordDim(the_geom), ST_SRID(the_geom), GeometryType(the_geom)
